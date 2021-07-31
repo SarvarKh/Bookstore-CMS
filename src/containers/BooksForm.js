12992cca07cfc876ec87aa9c96151d200782b1be
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-31 23:50:38 +0500

    Implement BooksForm.js

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
 function BooksForm() {
   return (
-    <div>
-      Hello from BooksForm!
-    </div>
+    <form>
+      <input type="text" placeholder="Book title" />
+
+      <select name="category">
+        <option value="default">--Please choose a category--</option>
+        {bookCategories.map((category) => (
+          <option key={category}>{category}</option>
+        ))}
+      </select>
+
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
