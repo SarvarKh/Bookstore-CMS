3f32b56c1dbfd3b47e62251559abfd77a4ca7790
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-01 00:24:50 +0500

    Initialize redux store and implement basics of src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import bookReducer from './reducers/books';
+
+const store = createStore(bookReducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,24 +1,44 @@
-import { CREATE_BOOK, REMOVE_BOOK, createBook, removeBook } from "../actions";
+import {
+  CREATE_BOOK, REMOVE_BOOK,
+} from '../actions';
 
-const bookReducer = (state, action) => {
-    switch (action.type) {
-        case CREATE_BOOK:
-            return [...state, {
-                id: action.book.id,
-                title: action.book.title,
-                category: action.book.category
-            }]
-        case REMOVE_BOOK:
-            return {
-                // Remove book logic to be added
-                id: action.book.id,
-                title: action.book.title,
-                category: action.book.category
-            }
-    
-        default:
-            state;
-    }
-}
+const initialState = [
+  {
+    id: 1,
+    title: 'Book title one',
+    category: 'Fantacy',
+  },
+  {
+    id: 2,
+    title: 'Book title two',
+    category: 'Fantacy',
+  },
+  {
+    id: 3,
+    title: 'Book title three',
+    category: 'Fantacy',
+  },
+];
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return [...state, {
+        id: action.book.id,
+        title: action.book.title,
+        category: action.book.category,
+      }];
+    case REMOVE_BOOK:
+      return {
+        // Remove book logic to be added
+        id: action.book.id,
+        title: action.book.title,
+        category: action.book.category,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default bookReducer;
